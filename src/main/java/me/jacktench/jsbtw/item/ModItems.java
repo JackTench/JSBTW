b21f479f981fdb8cc1550e2839c997e65ebb74a9
JackTench <79285604+JackTench@users.noreply.github.com>
2024-04-28 00:41:00 +0100

    Added Orb of Transcendence.

--- src/main/java/me/jacktench/jsbtw/item/ModItems.java
+++ src/main/java/me/jacktench/jsbtw/item/ModItems.java
@@ -1,14 +1,18 @@
 package me.jacktench.jsbtw.item;
 
 import me.jacktench.jsbtw.JSBTW;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, JSBTW.MODID);
+
+    public static final RegistryObject<Item> ORB = ITEMS.register("orb", () -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus)
     {
