967afb323dee5872a9e4d866117e351f541784a0
JackTench <79285604+JackTench@users.noreply.github.com>
2024-04-28 21:09:17 +0100

    Implement Starfall Blade.

--- src/main/java/me/jacktench/jsbtw/item/ModItems.java
+++ src/main/java/me/jacktench/jsbtw/item/ModItems.java
@@ -4,6 +4,8 @@ import me.jacktench.jsbtw.JSBTW;
 import me.jacktench.jsbtw.ModCreativeModeTab;
 import me.jacktench.jsbtw.item.custom.ItemOrb;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,6 +16,7 @@ public class ModItems
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, JSBTW.MODID);
 
     public static final RegistryObject<Item> ORB = ITEMS.register("orb", () -> new ItemOrb(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.MOD_TAB)));
+    public static final RegistryObject<Item> STARFALL_BLADE = ITEMS.register("starfall_blade", () -> new SwordItem(Tiers.NETHERITE, 3, -2.4F, (new Item.Properties()).tab(ModCreativeModeTab.MOD_TAB)));
 
     public static void register(IEventBus eventBus)
     {
