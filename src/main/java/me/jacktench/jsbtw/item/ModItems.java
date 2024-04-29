d43e39133618efcc5882412b59b2a111ea3adb54
JackTench <79285604+JackTench@users.noreply.github.com>
2024-04-29 17:59:16 +0100

    Add tiny coal.

--- src/main/java/me/jacktench/jsbtw/item/ModItems.java
+++ src/main/java/me/jacktench/jsbtw/item/ModItems.java
@@ -2,6 +2,7 @@ package me.jacktench.jsbtw.item;
 
 import me.jacktench.jsbtw.JSBTW;
 import me.jacktench.jsbtw.ModCreativeModeTab;
+import me.jacktench.jsbtw.item.custom.ItemFuel;
 import me.jacktench.jsbtw.item.custom.ItemOrb;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SwordItem;
@@ -19,6 +20,7 @@ public class ModItems
     public static final RegistryObject<Item> STARFALL_BLADE = ITEMS.register("starfall_blade", () -> new SwordItem(Tiers.NETHERITE, 3, -2.4F, (new Item.Properties()).tab(ModCreativeModeTab.MOD_TAB)));
     public static final RegistryObject<Item> BROKEN_STARFALL_BLADE = ITEMS.register("broken_starfall_blade", () -> new Item(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.MOD_TAB)));
     public static final RegistryObject<Item> CELESTIAL_NEXUS = ITEMS.register("celestial_nexus", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MOD_TAB)));
+    public static final RegistryObject<Item> TINY_COAL = ITEMS.register("tiny_coal", () -> new ItemFuel(new Item.Properties().tab(ModCreativeModeTab.MOD_TAB), 200));
 
     public static void register(IEventBus eventBus)
     {
