8b3c6f8d18761a363557e62bd843760177da0248
JackTench <79285604+JackTench@users.noreply.github.com>
2024-04-29 22:52:43 +0100

    Glowing glass implemented but not transparent...

---
+++ src/main/java/me/jacktench/jsbtw/block/custom/BlockGlowingGlass.java
@@ -0,0 +1,11 @@
+package me.jacktench.jsbtw.block.custom;
+
+import net.minecraft.world.level.block.GlassBlock;
+
+public class BlockGlowingGlass extends GlassBlock
+{
+    public BlockGlowingGlass(Properties properties)
+    {
+        super(properties);
+    }
+}
