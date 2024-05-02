99c4fd4aab427eb9264c377718084cc3a52fed7d
JackTench <79285604+JackTench@users.noreply.github.com>
2024-05-02 23:37:42 +0100

    Add helper method for registering block entities.

--- src/main/java/me/jacktench/jsbtw/block/ModBlockEntities.java
+++ src/main/java/me/jacktench/jsbtw/block/ModBlockEntities.java
@@ -2,10 +2,16 @@ package me.jacktench.jsbtw.block;
 
 import me.jacktench.jsbtw.JSBTW;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBlockEntities
 {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, JSBTW.MODID);
+
+    public static void register(IEventBus eventBus)
+    {
+        BLOCK_ENTITIES.register(eventBus);
+    }
 }
