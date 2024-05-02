bc49b86f824d981f5164ab2aefb5e76a937b3a75
JackTench <79285604+JackTench@users.noreply.github.com>
2024-05-02 23:40:26 +0100

    Register BlockEntity for Reflector.

--- src/main/java/me/jacktench/jsbtw/block/ModBlockEntities.java
+++ src/main/java/me/jacktench/jsbtw/block/ModBlockEntities.java
@@ -5,10 +5,14 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntities
 {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, JSBTW.MODID);
+
+    public static final RegistryObject<BlockEntityType<BlockEntityReflector>> REFLECTOR = BLOCK_ENTITIES.register("reflector",
+            () -> BlockEntityType.Builder.of(BlockEntityReflector::new, ModBlocks.REFLECTOR.get()).build(null));
 
     public static void register(IEventBus eventBus)
     {
