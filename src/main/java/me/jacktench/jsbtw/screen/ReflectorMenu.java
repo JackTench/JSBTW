cca0c7e0bb57112cdb980ede1021f149499056b2
JackTench <79285604+JackTench@users.noreply.github.com>
2024-05-02 19:21:13 +0100

    Menu constructor...

--- src/main/java/me/jacktench/jsbtw/screen/ReflectorMenu.java
+++ src/main/java/me/jacktench/jsbtw/screen/ReflectorMenu.java
@@ -1,14 +1,12 @@
 package me.jacktench.jsbtw.screen;
 
 import com.mojang.serialization.Decoder;
+import me.jacktench.jsbtw.block.ModBlocks;
 import me.jacktench.jsbtw.block.entity.BlockEntityReflector;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.inventory.SimpleContainerData;
+import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -32,17 +30,35 @@ public class ReflectorMenu extends AbstractContainerMenu
         blockEntity = (BlockEntityReflector) entity;
         this.level = inv.player.level;
         this.data = data;
+        addPlayerInventory(inv);
+        addPlayerHotbar(inv);
     }
 
     @Override
-    public ItemStack quickMoveStack(Player p_38941_, int p_38942_)
+    public ItemStack quickMoveStack(Player player, int p_38942_)
     {
         return null;
     }
 
     @Override
-    public boolean stillValid(Player p_38874_)
+    public boolean stillValid(Player player)
     {
-        return false;
+        return stillValid(ContainerLevelAccess.create(level, blockEntity.getBlockPos()), player, ModBlocks.REFLECTOR.get());
+    }
+
+    private void addPlayerInventory(Inventory playerInventory)
+    {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; i < 9; ++l) {
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory)
+    {
+        for (int i = 0; i < 9; ++i) {
+            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
+        }
     }
 }
