2b6ec387410316db2faede6e891363d6c4dc3074
JackTench <79285604+JackTench@users.noreply.github.com>
2024-05-02 23:36:37 +0100

    Create DeferredRegister for block entities.

---
+++ src/main/java/me/jacktench/jsbtw/block/ModBlockEntities.java
@@ -0,0 +1,11 @@
+package me.jacktench.jsbtw.block;
+
+import me.jacktench.jsbtw.JSBTW;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlockEntities
+{
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, JSBTW.MODID);
+}
